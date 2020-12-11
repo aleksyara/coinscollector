7938ac4d6ca16b1437a1a68f4d1b51f9996e0ac5
Aleksei Yarantsev <aleksyara@Alekseis-MacBook-Pro.local>
2020-12-11 15:22:18 -0800

    add trading mpdel

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 
 # Create your models here.
 
+DEALS = (
+    ('E', 'Exchange'),
+    ('S', 'Sale'),
+    ('F', 'Free')
+)
+
 class Coin(models.Model):
     material = models.CharField(max_length = 100)
     country = models.CharField(max_length = 100)
@@ -16,3 +22,18 @@ class Coin(models.Model):
 # method to display a new Coin
     def get_absolute_url(self):
         return reverse('detail', kwargs={'coin_id': self.id})
+
+
+# Add new Feeding model below Cat model
+class Trading(models.Model):
+    date = models.DateField()
+    deals = models.CharField(
+        max_length=1,
+        choices=DEALS,
+        default=DEALS[0][0] 
+        )
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_deal_display()} on {self.date}"
